f6c96935efff06145aac24868a3432a759420548
Azraelbazrael <casilanazrael@gmail.com>
2025-05-14 13:23:06 -0500

    vnvnc

--- Global/global.gd
+++ Global/global.gd
@@ -1,14 +1,14 @@
 extends Node
-#class_name Global
-signal init_battle // initiates battles
-signal spell_options //shows spells player has
-signal be_selected(this_target: Node2D) // signals that the target has been chosen for attacks
-signal choose_target // gives player option to choose targets
-signal wave_complete // when a battle is completed
-signal game_over // when all players died 
-signal casted(SpellData) // holds the spell selected to be referenced later
 
-#signal spelless 
+signal init_battle ## initiates battles
+signal spell_options ##shows spells player has
+signal be_selected(this_target: Node2D) ## signals that the target has been chosen for attacks
+signal choose_target ## gives player option to choose targets
+signal wave_complete ## when a battle is completed
+signal game_over ## when all players died 
+signal casted(SpellData) ## holds the spell selected to be referenced later
+
+
 
 
 var current_turn: Node2D
